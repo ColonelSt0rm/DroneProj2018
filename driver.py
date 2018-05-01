473f650bdba6ca44358ce7873497add0dc0b5896
Owen Davidson <owenmedia@outlook.com>
2018-05-01 12:20:39 -0600

    separated driver and multiwii

--- driver.py
+++ driver.py
@@ -1,19 +1,14 @@
 import pymultiwii import MultiWii
 import ESC
-
+import multiwii
 
 
 
 if __name__ == "__main__":
-
+    
     ESC.calibrate()
-
-    board = MultiWii("/dev/tty.SLAB_USBtoUART")
     try:
-        while True:
-            board.getData(MultiWii.ATTITUDE)
-            message = "angx = {:+.2f} \t angy = {:+.2f} \t heading = {:+.2f} \t elapsed = {:+.4f} \t".format(float(board.attitude['angx']),float(board.attitude['angy']),float(board.attitude['heading']),float(board.attitude['elapsed']))
-            stdout.write("\r%s" % message )
-            stdout.flush()
-    except Exception,error:
-        print "Error on Main: "+str(error)
+        thread.start_new_thread(ESC.manual_drive())
+        thread.start_new_thread(multiwii.runAttitude())
+    except:
+        print "Error: unable to start thread"
---
+++ multiwii.py
@@ -0,0 +1,12 @@
+from pymultiwii import MultiWii
+
+def runAttitude():
+    board = MultiWii("/dev/tty.SLAB_USBtoUART")
+    try:
+        while True:
+            board.getData(MultiWii.ATTITUDE)
+            message = "angx = {:+.2f} \t angy = {:+.2f} \t heading = {:+.2f} \t elapsed = {:+.4f} \t".format(float(board.attitude['angx']),float(board.attitude['angy']),float(board.attitude['heading']),float(board.attitude['elapsed']))
+            stdout.write("\r%s" % message )
+            stdout.flush()
+    except Exception,error:
+        print "Error on Main: "+str(error)
